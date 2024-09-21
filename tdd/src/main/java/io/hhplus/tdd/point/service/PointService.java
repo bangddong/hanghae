81b6518bcbda0900565866806b59e4a7ff661d46
bangddong <gusehdqkd@naver.com>
2024-09-21 19:30:52 +0900

    feat : API 기본 기능 구현

---
+++ tdd/src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -0,0 +1,44 @@
+package io.hhplus.tdd.point.service;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import io.hhplus.tdd.point.dto.PointHistory;
+import io.hhplus.tdd.point.dto.UserPoint;
+import io.hhplus.tdd.point.type.TransactionType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PointService {
+
+    private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
+    public UserPoint getPoint(long id) {
+        return userPointTable.selectById(id);
+    }
+
+    public List<PointHistory> getHistories(long id) {
+        return pointHistoryTable.selectAllByUserId(id);
+    }
+
+    public UserPoint charge(long id, long amount) {
+        UserPoint userPoint = userPointTable.selectById(id);
+        userPointTable.insertOrUpdate(id, userPoint.point() + amount);
+        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+        return userPointTable.selectById(id);
+    }
+
+    public UserPoint use(long id, long amount) {
+        UserPoint userPoint = userPointTable.selectById(id);
+        if (userPoint.point() < amount) {
+            throw new IllegalArgumentException("잔액이 부족합니다.");
+        }
+        userPointTable.insertOrUpdate(id, userPoint.point() - amount);
+        pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
+        return userPointTable.selectById(id);
+    }
+}
