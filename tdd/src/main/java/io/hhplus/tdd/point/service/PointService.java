ee564e683b221eceb517f08e8aa4a125f576360a
dhbang <gusehdqkd@naver.com>
2024-09-24 22:10:54 +0900

    style : 코드 포맷터로 인한 포맷 변경

--- tdd/src/main/java/io/hhplus/tdd/point/service/PointService.java
+++ tdd/src/main/java/io/hhplus/tdd/point/service/PointService.java
@@ -1,5 +1,9 @@
 package io.hhplus.tdd.point.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import io.hhplus.tdd.point.dto.PointHistoryResponse;
 import io.hhplus.tdd.point.dto.UserPointResponse;
 import io.hhplus.tdd.point.entity.UserPoint;
@@ -9,38 +13,35 @@ import io.hhplus.tdd.point.service.reader.HistoryReader;
 import io.hhplus.tdd.point.service.reader.PointReader;
 import io.hhplus.tdd.point.type.TransactionType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PointService {
 
-    private final PointReader pointReader;
-    private final HistoryReader historyReader;
-    private final PointManager pointManager;
-    private final HistoryManager historyManager;
+	private final PointReader pointReader;
+	private final HistoryReader historyReader;
+	private final PointManager pointManager;
+	private final HistoryManager historyManager;
 
-    public UserPointResponse getPoint(long id) {
-        return UserPointResponse.fromEntity(pointReader.read(id));
-    }
+	public UserPointResponse getPoint(long id) {
+		return UserPointResponse.fromEntity(pointReader.read(id));
+	}
 
-    public List<PointHistoryResponse> getHistories(long id) {
-        return historyReader.read(id).stream()
-                .map(PointHistoryResponse::fromEntity)
-                .toList();
-    }
+	public List<PointHistoryResponse> getHistories(long id) {
+		return historyReader.read(id).stream()
+			.map(PointHistoryResponse::fromEntity)
+			.toList();
+	}
 
-    public UserPointResponse charge(long id, long amount) {
-        final UserPoint updatedUserPoint = pointManager.charge(id, amount);
-        historyManager.append(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
-        return UserPointResponse.fromEntity(updatedUserPoint);
-    }
+	public UserPointResponse charge(long id, long amount) {
+		final UserPoint updatedUserPoint = pointManager.charge(id, amount);
+		historyManager.append(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+		return UserPointResponse.fromEntity(updatedUserPoint);
+	}
 
-    public UserPointResponse use(long id, long amount) {
-        final UserPoint updatedUserPoint = pointManager.use(id, amount);
-        historyManager.append(id, amount, TransactionType.USE, System.currentTimeMillis());
-        return UserPointResponse.fromEntity(updatedUserPoint);
-    }
+	public UserPointResponse use(long id, long amount) {
+		final UserPoint updatedUserPoint = pointManager.use(id, amount);
+		historyManager.append(id, amount, TransactionType.USE, System.currentTimeMillis());
+		return UserPointResponse.fromEntity(updatedUserPoint);
+	}
 }
--- tdd/src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
+++ tdd/src/test/java/io/hhplus/tdd/point/service/PointServiceTest.java
@@ -1,5 +1,17 @@
 package io.hhplus.tdd.point.service;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import io.hhplus.tdd.point.dto.PointHistoryResponse;
 import io.hhplus.tdd.point.dto.UserPointResponse;
 import io.hhplus.tdd.point.entity.PointHistory;
@@ -9,146 +21,136 @@ import io.hhplus.tdd.point.service.manager.PointManager;
 import io.hhplus.tdd.point.service.reader.HistoryReader;
 import io.hhplus.tdd.point.service.reader.PointReader;
 import io.hhplus.tdd.point.type.TransactionType;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 class PointServiceTest {
 
-    @Mock
-    private PointReader pointReader;
+	@Mock
+	private PointReader pointReader;
 
-    @Mock
-    private HistoryReader historyReader;
+	@Mock
+	private HistoryReader historyReader;
 
-    @Mock
-    private PointManager pointManager;
+	@Mock
+	private PointManager pointManager;
 
-    @Mock
-    private HistoryManager historyManager;
+	@Mock
+	private HistoryManager historyManager;
 
-    @InjectMocks
-    private PointService pointService;
+	@InjectMocks
+	private PointService pointService;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
 
-    @Test
-    void 유저_포인트_조회() {
-        // Given
-        long userId = 1L;
+	@Test
+	void 유저_포인트_조회() {
+		// Given
+		long userId = 1L;
 
-        UserPoint userPoint = new UserPoint(userId, 0L, System.currentTimeMillis());
+		UserPoint userPoint = new UserPoint(userId, 0L, System.currentTimeMillis());
 
-        when(pointReader.read(userId)).thenReturn(userPoint);
+		when(pointReader.read(userId)).thenReturn(userPoint);
 
-        // When
-        UserPointResponse result = pointService.getPoint(userId);
+		// When
+		UserPointResponse result = pointService.getPoint(userId);
 
-        // Then
-        verify(pointReader, times(1)).read(userId);
+		// Then
+		verify(pointReader, times(1)).read(userId);
 
-        assertEquals(userId, result.id());
-        assertEquals(0, result.point());
-    }
+		assertEquals(userId, result.id());
+		assertEquals(0, result.point());
+	}
 
-    @Test
-    void 유저_포인트_내역_조회() {
-        // Given
-        long userId = 1L;
-        long chargeAmount = 1000L;
-        long useAmount = 100L;
+	@Test
+	void 유저_포인트_내역_조회() {
+		// Given
+		long userId = 1L;
+		long chargeAmount = 1000L;
+		long useAmount = 100L;
 
-        List<PointHistory> pointHistories = Arrays.asList(
-                new PointHistory(1L, userId, chargeAmount, TransactionType.CHARGE, System.currentTimeMillis()),
-                new PointHistory(2L, userId, useAmount, TransactionType.USE, System.currentTimeMillis())
-        );
+		List<PointHistory> pointHistories = Arrays.asList(
+			new PointHistory(1L, userId, chargeAmount, TransactionType.CHARGE, System.currentTimeMillis()),
+			new PointHistory(2L, userId, useAmount, TransactionType.USE, System.currentTimeMillis())
+		);
 
-        when(historyReader.read(userId)).thenReturn(pointHistories);
+		when(historyReader.read(userId)).thenReturn(pointHistories);
 
-        // When
-        List<PointHistoryResponse> result = pointService.getHistories(userId);
+		// When
+		List<PointHistoryResponse> result = pointService.getHistories(userId);
 
-        // Then
-        verify(historyReader, times(1)).read(userId);
+		// Then
+		verify(historyReader, times(1)).read(userId);
 
-        assertEquals(pointHistories.size(), result.size());
-        assertEquals(chargeAmount, result.get(0).amount());
-        assertEquals("CHARGE", result.get(0).type());
-        assertEquals(useAmount, result.get(1).amount());
-        assertEquals("USE", result.get(1).type());
-    }
+		assertEquals(pointHistories.size(), result.size());
+		assertEquals(chargeAmount, result.get(0).amount());
+		assertEquals("CHARGE", result.get(0).type());
+		assertEquals(useAmount, result.get(1).amount());
+		assertEquals("USE", result.get(1).type());
+	}
 
-    @Test
-    void 유저_포인트_충전() {
-        // Given
-        long userId = 1L;
-        long chargeAmount = 1000L;
+	@Test
+	void 유저_포인트_충전() {
+		// Given
+		long userId = 1L;
+		long chargeAmount = 1000L;
 
-        UserPoint updatedUserPoint = new UserPoint(userId, chargeAmount, System.currentTimeMillis());
+		UserPoint updatedUserPoint = new UserPoint(userId, chargeAmount, System.currentTimeMillis());
 
-        when(pointManager.charge(userId, chargeAmount))
-                .thenReturn(updatedUserPoint);
+		when(pointManager.charge(userId, chargeAmount))
+			.thenReturn(updatedUserPoint);
 
-        // When
-        UserPointResponse result = pointService.charge(userId, chargeAmount);
+		// When
+		UserPointResponse result = pointService.charge(userId, chargeAmount);
 
-        // Then
-        verify(pointManager, times(1))
-                .charge(eq(userId), eq(chargeAmount));
-        verify(historyManager, times(1))
-                .append(eq(userId), eq(chargeAmount), eq(TransactionType.CHARGE), anyLong());
+		// Then
+		verify(pointManager, times(1))
+			.charge(eq(userId), eq(chargeAmount));
+		verify(historyManager, times(1))
+			.append(eq(userId), eq(chargeAmount), eq(TransactionType.CHARGE), anyLong());
 
-        assertEquals(chargeAmount, result.point());
-    }
+		assertEquals(chargeAmount, result.point());
+	}
 
-    @Test
-    void 유저_포인트_사용_성공() {
-        // Given
-        long userId = 1L;
-        long useAmount = 500L;
+	@Test
+	void 유저_포인트_사용_성공() {
+		// Given
+		long userId = 1L;
+		long useAmount = 500L;
 
-        UserPoint updatedUserPoint = new UserPoint(userId, useAmount, System.currentTimeMillis());
+		UserPoint updatedUserPoint = new UserPoint(userId, useAmount, System.currentTimeMillis());
 
-        when(pointManager.use(userId, useAmount)).thenReturn(updatedUserPoint);
+		when(pointManager.use(userId, useAmount)).thenReturn(updatedUserPoint);
 
-        // When
-        UserPointResponse result = pointService.use(userId, useAmount);
+		// When
+		UserPointResponse result = pointService.use(userId, useAmount);
 
-        // Then
-        verify(pointManager, times(1))
-                .use(eq(userId), eq(useAmount));
-        verify(historyManager, times(1))
-                .append(eq(userId), eq(useAmount), eq(TransactionType.USE), anyLong());
+		// Then
+		verify(pointManager, times(1))
+			.use(eq(userId), eq(useAmount));
+		verify(historyManager, times(1))
+			.append(eq(userId), eq(useAmount), eq(TransactionType.USE), anyLong());
 
-        assertEquals(useAmount, result.point());
-    }
+		assertEquals(useAmount, result.point());
+	}
 
-    @Test
-    void 유저_포인트_사용_잔액부족() {
-        // Given
-        long userId = 1L;
-        long useAmount = 1500L;
+	@Test
+	void 유저_포인트_사용_잔액부족() {
+		// Given
+		long userId = 1L;
+		long useAmount = 1500L;
 
-        when(pointManager.use(userId, useAmount)).thenThrow(new IllegalArgumentException("잔액이 부족합니다."));
+		when(pointManager.use(userId, useAmount)).thenThrow(new IllegalArgumentException("잔액이 부족합니다."));
 
-        // When & Then
-        IllegalArgumentException exception =
-                assertThrows(IllegalArgumentException.class, () -> pointService.use(userId, useAmount));
+		// When & Then
+		IllegalArgumentException exception =
+			assertThrows(IllegalArgumentException.class, () -> pointService.use(userId, useAmount));
 
-        verify(pointManager, times(1)).use(eq(userId), eq(useAmount));
-        verify(historyManager, never()).append(anyLong(), anyLong(), any(), anyLong());
+		verify(pointManager, times(1)).use(eq(userId), eq(useAmount));
+		verify(historyManager, never()).append(anyLong(), anyLong(), any(), anyLong());
 
-        assertEquals("잔액이 부족합니다.", exception.getMessage());
-    }
+		assertEquals("잔액이 부족합니다.", exception.getMessage());
+	}
+
 }
