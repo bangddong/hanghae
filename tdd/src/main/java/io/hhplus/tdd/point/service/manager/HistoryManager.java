2505b7a0c02ac97086b0eb968c4c1b3f1e5bf6fe
dhbang <gusehdqkd@naver.com>
2024-09-24 22:10:54 +0900

    style : 코드 포맷터로 인한 포맷 변경

--- tdd/src/main/java/io/hhplus/tdd/point/service/manager/HistoryManager.java
+++ tdd/src/main/java/io/hhplus/tdd/point/service/manager/HistoryManager.java
@@ -1,18 +1,19 @@
 package io.hhplus.tdd.point.service.manager;
 
+import org.springframework.stereotype.Component;
+
 import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.point.type.TransactionType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class HistoryManager {
 
-        private final PointHistoryTable pointHistoryTable;
+	private final PointHistoryTable pointHistoryTable;
 
-        public void append(long id, long amount, TransactionType transactionType, long timestamp) {
-            pointHistoryTable.insert(id, amount, transactionType, timestamp);
-        }
+	public void append(long id, long amount, TransactionType transactionType, long timestamp) {
+		pointHistoryTable.insert(id, amount, transactionType, timestamp);
+	}
 
 }
