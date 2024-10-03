738f938d80cf7d0708a1cc56824194c06e7b41f6
bangddong <gusehdqkd@naver.com>
2024-10-04 01:48:25 +0900

    chore : data.sql 수정  - ID값은 자동 증가니 따로 insert문에 적지 않는다.

--- architecture/src/main/resources/data.sql
+++ architecture/src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- Lecture 테이블에 기초 데이터 삽입
-INSERT INTO lecture (id, tutor, subject) VALUES (1, '이석범', 'JAVA');
-INSERT INTO lecture (id, tutor, subject) VALUES (2, '렌', 'SPRING');
-INSERT INTO lecture (id, tutor, subject) VALUES (3, '허재', 'REACT');
+INSERT INTO lecture (tutor, subject) VALUES ('이석범', 'JAVA');
+INSERT INTO lecture (tutor, subject) VALUES ('렌', 'SPRING');
+INSERT INTO lecture (tutor, subject) VALUES ('허재', 'REACT');
 
 -- Schedule 테이블에 기초 데이터 삽입
-INSERT INTO schedule (id, lecture_id, capacity, date, count) VALUES (1, 1, 30, '2024-10-01', 29);
-INSERT INTO schedule (id, lecture_id, capacity, date, count) VALUES (2, 2, 30, '2024-10-05', 30);
-INSERT INTO schedule (id, lecture_id, capacity, date, count) VALUES (3, 3, 30, '2024-10-10', 29);
+INSERT INTO schedule (lecture_id, capacity, date, count) VALUES (1, 30, '2024-10-01', 0);
+INSERT INTO schedule (lecture_id, capacity, date, count) VALUES ( 2, 30, '2024-10-05', 0);
+INSERT INTO schedule (lecture_id, capacity, date, count) VALUES ( 3, 30, '2024-10-10', 0);
